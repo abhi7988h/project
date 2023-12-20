da77d322f747a965dba252b51223195c9b7a07c4
abhi7988h <abhiarj329@gmail.com>
2023-12-20 19:51:17 +0530

    index html

---
+++ call.js
@@ -0,0 +1,25 @@
+var box=document.getElementById("calc-display");
+function toScreen(x){
+    box.value+=x;
+    if(x==='c'){
+        box.value='';
+    }
+}
+
+function answer(){
+    x=box.value;
+    x=eval(x);
+    box.value=x;
+}
+
+function sqr(){
+    x=box.value;
+    x=eval(x*x);
+    box.value=x;
+}
+function backspace(){
+    var num=box.value;
+    var len=num.length-1;
+    var newNum=num.substring(0,len);
+    box.value=newNum
+}
